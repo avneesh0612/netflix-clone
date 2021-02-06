3e7b0cf38aeca0cca27623af9f5ad828179a3b81
Avneesh Agarwal <76690419+avneesh0612@users.noreply.github.com>
2021-02-06 23:02:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,5 @@
 import firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAf0Gm-cjgGCYOymPDfrijDJ7MtL93Yx1c",
-  authDomain: "netflix-clone-fc00d.firebaseapp.com",
-  projectId: "netflix-clone-fc00d",
-  storageBucket: "netflix-clone-fc00d.appspot.com",
-  messagingSenderId: "26972300119",
-  appId: "1:26972300119:web:b3c6b38dff860eb52a5412",
-  measurementId: "G-4NX86K65QQ",
-};
+//firebase config
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
